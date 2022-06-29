39a53da311440e7529c91ba6f710f773475d9444
Erikson <chris.s.erickson@gmail.com>
2022-06-29 15:48:11 -0400

    general question layout and event listener added to start

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,11 +2,14 @@
 // pageContent - main body id page-content
 // timerDiv - div in header - timer
 // timerClock - p under div in header
-// 
+var mainPage = document.querySelector("#landing");
+console.log(mainPage);
 
 var timeLeft = 5;
 var timedInterval;
 var startBtn = document.querySelector("#goButton");
+var corrIncNote = document.createElement("button");
+corrIncNote.innerHTML = "Answer!"
 
 // selecting timer parent "div"
 var timerDiv = document.getElementById("timer");
@@ -37,8 +40,11 @@ startBtn.addEventListener("click", function (){
 
 });
 
+// function to click high score box - prompt to call the saved data in alert or prompt
+
+
 // QUESTION Create Block
-var questionIndex = 0;
+var questionIndex = [];
 
 // questionsArr[questionIndex]
 
@@ -59,96 +65,100 @@ var questionsArr = [
         correctAnswer: "c" 
     },
     {
-        question: "some question here?",
+        question: "Question#4?",
         answers: ["m", "n", "o", "p"],
         correctAnswer: "d" 
     },
     {
-        question: "some question here?",
+        question: "Question#5?",
         answers: ["q", "r", "s", "t"],
         correctAnswer: "a" 
-    }
-]
+    },
+];
 
+// function for answering correct
+//  add point to score, give a correct propmpt, chime sound (if possible)
+// move to next question (function for each question?)
+
+// function for answering incorrect
+//  gong sound ( if possible), incorrect prompt, move to next question, subtract time from timer
 
 
 // question #1
 // select the section
-var questionSect = document.querySelector("#question");
+// add data attribute to question
+// 
+startBtn.addEventListener("click", function (){
 
-// create h1 question block
-var questionNum = document.createElement("h1");
-questionNum.textContent=
-    questionsArr.index= 0;
+    // make landing page disapear
+    function backgroundClear(){
+        mainPage.style.height = "10px";
+        mainPage.style.visibility="hidden";
+    };
+    backgroundClear();
+    // aiming on section
+    var questionSect = document.querySelector("#question");
+
+    // create 2 divs for text and question section
+    var questionNum = document.createElement("div");
+    var answerBlock = document.createElement("div");
+    answerBlock.className = ("answerBlock"); 
+
+    // append first 2 divs
+    questionSect.appendChild(questionNum);
+    questionSect.appendChild(answerBlock);
+
+    // create h1 - question text
+    var questionText = document.createElement("h1");
+    questionText.textContent = " This is a placeholder for question #1."
+
+    // append h1 into question div
+    questionNum.appendChild(questionText);
+
+    // create 2 div for answer block
+    // create choice div
+    var multiChoice = document.createElement("div");
+    multiChoice.className = ("questionActual");
+    // create response div
+    var answerResp = document.createElement("div");
+    answerResp.className = ("answerResp");
+    
+    // append answer and response to block
+    answerBlock.appendChild(multiChoice);
+    answerBlock.appendChild(answerResp);
+
+    // create buttons for choice
+    var choiceA = document.createElement("button");
+    choiceA.className = ("choiceA");
+    choiceA.textContent = ("Option A");
+
+    var choiceB = document.createElement("button");
+    choiceB.className = ("choiceB");
+    choiceB.textContent = ("Option B");
+
+    var choiceC = document.createElement("button");
+    choiceC.className = ("choiceC");
+    choiceC.textContent = ("Option C");
+
+    var choiceD = document.createElement("button");
+    choiceD.className = ("choiceD");
+    choiceD.textContent = ("Option D");
+    
+    // append choices under multichoice div
+    multiChoice.appendChild(choiceA);
+    multiChoice.appendChild(choiceB);
+    multiChoice.appendChild(choiceC);
+    multiChoice.appendChild(choiceD);
+
+    // create response p
+    var corrIncNote = document.createElement("p");
+    corrIncNote.textContent= ("Answer!");
+
+    // append response p
+    answerResp.appendChild(corrIncNote);
+});
 
 
-// create ordered  buttons for answers
-var multiChoice = document.createElement("div");
-multiChoice.className = ("questionActual");
 
-var choiceA = document.createElement("button");
-choiceA.className = ("choiceA");
-choiceA.textContent = ("Option A");
-
-var choiceB = document.createElement("button");
-choiceB.className = ("choiceB");
-choiceB.textContent = ("Option B");
-
-var choiceC = document.createElement("button");
-choiceC.className = ("choiceC");
-choiceC.textContent = ("Option C");
-
-var choiceD = document.createElement("button");
-choiceD.className = ("choiceD");
-choiceD.textContent = ("Option D");
-questionSect.appendChild(questionNum);
-questionSect.appendChild(multiChoice);
-multiChoice.appendChild(choiceA);
-multiChoice.appendChild(choiceB);
-multiChoice.appendChild(choiceC);
-multiChoice.appendChild(choiceD);
-
-
-
-var questionIndex = 0;
-
-// questionsArr[questionIndex]
-
-var questionsArr = [
-    {
-        question: "some question here?",
-        answers: ["a", "b", "c", "d"],
-        correctAnswer: "a" 
-    },
-    {
-        question: "some question here?",
-        answers: ["a", "b", "c", "d"],
-        correctAnswer: "a" 
-    },
-    {
-        question: "some question here?",
-        answers: ["a", "b", "c", "d"],
-        correctAnswer: "a" 
-    }
-]
-
-
-// function display for appended questions?
-
-// variable question 1, ...
-// var questionSection = function questionAdd(){
-//     //  function questionSection(){
-//     //     // turn body into a variable
-//         var pageContent = document.getElementById ("page-content");
-//         var pageContent = document.createElement ("section");
-//         var questionBlock = document.getElementById("question");
-//         var questionNum = document.createElement ("h1");
-//         pageContent.appendChild(questionBlock);
-//         questionBlock.appendChild(questionNum);
-//         console.log(pageContent);
-//     // }
-
-//     // questionSection();
-// };
-
-// questionAdd();
+// function to save scores, track high score, add initials to save
+// button to go back to start/landing page to start over
