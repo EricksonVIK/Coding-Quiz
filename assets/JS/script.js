d355a8621856b7452a1164a9af1bc4e3b60679fd
Erikson <chris.s.erickson@gmail.com>
2022-06-28 22:12:25 -0400

    fixed timer and started question array

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,6 +4,10 @@
 // timerClock - p under div in header
 // 
 
+var timeLeft = 5;
+var timedInterval;
+var startBtn = document.querySelector("#goButton");
+
 // selecting timer parent "div"
 var timerDiv = document.getElementById("timer");
 // creating timer "p"
@@ -13,17 +17,70 @@ timerDiv.appendChild(timerClock);
 
 timerDiv.appendChild(timerClock);
 
-timerClock.textContent = (countdown());
+// timerClock.textContent = (countdown());
+startBtn.addEventListener("click", function (){
+    
+    timedInterval = setInterval(function (){
+        if (timeLeft > 1 ){
+            timerClock.innerHTML = timeLeft + " seconds remaining ";
+        } else if (timeLeft === 1) {
+            timerClock.innerHTML = timeLeft + " second remaining ";
+        } else {
+            // go to final page
+            console.log('hit')
+            timerClock.innerHTML = "Time's Up!"
+            clearInterval(timedInterval);
+            return
+        };
+        timeLeft --;
+    }, 1000);
+
+});
 
 // QUESTION Create Block
+var questionIndex = 0;
+
+// questionsArr[questionIndex]
+
+var questionsArr = [
+    {
+        question: "Question#1?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "a" 
+    },
+    {
+        question: "Question#2?",
+        answers: ["e", "f", "g", "h"],
+        correctAnswer: "b" 
+    },
+    {
+        question: "Question#3?",
+        answers: ["i", "j", "k", "l"],
+        correctAnswer: "c" 
+    },
+    {
+        question: "some question here?",
+        answers: ["m", "n", "o", "p"],
+        correctAnswer: "d" 
+    },
+    {
+        question: "some question here?",
+        answers: ["q", "r", "s", "t"],
+        correctAnswer: "a" 
+    }
+]
+
+
+
+// question #1
 // select the section
 var questionSect = document.querySelector("#question");
 
 // create h1 question block
 var questionNum = document.createElement("h1");
-questionNum.textContent=(
-    "This is where the question will end up"
-    );
+questionNum.textContent=
+    questionsArr.index= 0;
+
 
 // create ordered  buttons for answers
 var multiChoice = document.createElement("div");
@@ -50,33 +107,32 @@ multiChoice.appendChild(choiceA);
 multiChoice.appendChild(choiceB);
 multiChoice.appendChild(choiceC);
 multiChoice.appendChild(choiceD);
-// add elements
-// pageContent.appendChild(questionBlock);
 
-var startButton = document.querySelector("#go-Button");
-startButton.addEventListener("submit", countdown());
-console.dir("startButton");
 
-// timer function
-function countdown(){
 
-    var timeLeft=60;
+var questionIndex = 0;
 
-    var timedInterval = setInterval(function (){
-    if (timeLeft > 1 ){
-        timerClock.textContent = timeLeft + " seconds remaining ";
-        timeLeft --;
-    } else if (timeLeft === 1) {
-        timerClock.textContent = timeLeft + " second remaining ";
-    } else {
-        // go to final page
-        timerClock.textContent = ""};
-        clearInterval(timedInterval);
-    }, 1000);
+// questionsArr[questionIndex]
 
-};
+var questionsArr = [
+    {
+        question: "some question here?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "a" 
+    },
+    {
+        question: "some question here?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "a" 
+    },
+    {
+        question: "some question here?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "a" 
+    }
+]
 
-countdown();
+
 // function display for appended questions?
 
 // variable question 1, ...
